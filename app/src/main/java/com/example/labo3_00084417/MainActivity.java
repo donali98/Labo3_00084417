e92ca6e55e53a88b9544dd374120f158534cf257
Donali <00084417@uca.edu.sv>
2019-03-22 22:37:55 -0600

    Terminado Ejercicio

--- app/src/main/java/com/example/labo3_00084417/MainActivity.java
+++ app/src/main/java/com/example/labo3_00084417/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.labo3_00084417;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
-    Button submit_action;
+    Button btnSend;
+    JSONObject info;
+    EditText txtUser,txtPass,txtEmail,txtGender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submit_action = findViewById(R.id.submit_action);
-        submit_action.setOnClickListener(new View.OnClickListener() {
+        btnSend = findViewById(R.id.btnSend);
+        txtUser = findViewById(R.id.txtUser);
+        txtPass = findViewById(R.id.txtPass);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtGender = findViewById(R.id.txtGender);
+
+        info = new JSONObject();
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent m_intent = new Intent(MainActivity.this,NewActivity.class);
-                m_intent.putExtra(AppConstant.TEXT1_KEY,"Texto desde MainActivity");
-                startActivity(m_intent);
+                try {
+                    info.put("username",txtUser.getText().toString());
+                    info.put("password",txtPass.getText().toString());
+                    info.put("email",txtEmail.getText().toString());
+                    info.put("gender",txtGender.getText().toString());
+                    m_intent.putExtra(AppConstant.TEXT1_KEY,  info.toString());
+                    startActivity(m_intent);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
--- app/src/main/java/com/example/labo3_00084417/NewActivity.java
+++ app/src/main/java/com/example/labo3_00084417/NewActivity.java
@@ -6,38 +6,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 
 public class NewActivity extends AppCompatActivity {
 
-    TextView sText1;
-    Button mSubmitAction;
-    String textAux = "Texto Quemado";
-
+    TextView txtUserResult,txtPassResult,txtEmailResult,txtGenderResult;
+    Button btnSend;
+    JSONObject info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
-        sText1 = findViewById(R.id.prueba_text);
-        mSubmitAction = findViewById(R.id.btnShare);
+        btnSend = findViewById(R.id.btnSendNew);
+        txtUserResult = findViewById(R.id.txtNameResult);
+        txtPassResult = findViewById(R.id.txtPasswordResult);
+        txtEmailResult = findViewById(R.id.txtEmailResult);
+        txtGenderResult = findViewById(R.id.txtGenderResult);
 
 
         //Si la activity fue levantada por el intent
         Intent new_intent = getIntent();
 
-
         if(new_intent!=null){
-            textAux = new_intent.getStringExtra(AppConstant.TEXT1_KEY);
+            String stringJson = new_intent.getExtras().getString(AppConstant.TEXT1_KEY);
+            try {
+                info = new JSONObject(stringJson);
+                txtUserResult.setText(info.getString("username"));
+                txtPassResult.setText(info.getString("password"));
+                txtEmailResult.setText(info.getString("email"));
+                txtGenderResult.setText(info.getString("gender"));
+
+
+                //Log.i("newActivityLog",info.getString("username"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-        sText1.setText(textAux);
-        mSubmitAction.setOnClickListener(new View.OnClickListener() {
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Enviando implicitamente
                 Intent mIntent2 = new Intent();
                 mIntent2.setAction(Intent.ACTION_SEND);
                 mIntent2.setType("text/plain");
-                mIntent2.putExtra(Intent.EXTRA_TEXT,textAux);
+                mIntent2.putExtra(Intent.EXTRA_TEXT,"Texto");
                 startActivity(mIntent2);
             }
         });
